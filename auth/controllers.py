32cb77b97b57c7230c0920f9522e190e3a16b416
Tiago Onofre <taraujonofre@gmail.com>
2021-07-31 20:19:25 -0300

    Service and exceptions of auth module

--- auth/controllers.py
+++ auth/controllers.py
@@ -1,16 +1,74 @@
 from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from rest_framework import status
+
+from auth.models import BearerAuthentication
+from auth.services import AuthService
+from auth.exceptions import *
 
 class AuthController(object):
 
+     # SERVICE LAYER CALL
+    def validate_business_logic(self, request):
+
+        service = AuthService(request['username'], request['password'])
+
+        if not service.validate_username():
+            return (False, InvalidUsername)
+
+        if not service.validate_password():
+            return (False, InvalidPassword)
+
+        if not service.validate_user_password():
+            return (False, InvalidWrongPassword)
+
+        # else
+        return (True, Exception)
+
+
     def authenticate(self, auth_request, request):
- 
+
         serializer = auth_request.serializer_class(data=request.data, context={'request' : request})
-        serializer.is_valid(raise_exception=True)
 
-        # User exist's => Retrieve
-        user = serializer.validated_data['user']
+        if serializer.is_valid():
 
-        # Retrieve or generate a token
-        token, created = Token.objects.get_or_create(user=user)
+            validated, excepted = self.validate_business_logic(serializer.validated_data)
 
-        return token, user
+            if validated:
+
+                # User exist's => Retrieve
+                user = serializer.validated_data['user']
+
+                # Retrieve or generate a token
+                token, created = Token.objects.get_or_create(user=user)
+
+                return Response(status= status.HTTP_200_OK, data= {
+                    'user-id' : user.pk,
+                    'token' : token.key,
+                    'token-type' : BearerAuthentication.keyword,
+                })
+
+            else:
+                try:
+                    raise excepted
+                except InvalidUsername:
+                    return Response(status= status.HTTP_406_NOT_ACCEPTABLE, data= {
+                        'error' : 'Usuário não existente.',
+                        'status' : 406,
+                    })
+                except InvalidPassword:
+                    return Response(status= status.HTTP_400_BAD_REQUEST, data= {
+                        'error' : 'Senha inválida.',
+                        'status' : 400,
+                    })
+                except InvalidWrongPassword:
+                    return Response(status= status.HTTP_401_UNAUTHORIZED, data= {
+                        'error' : 'Senha incorreta.',
+                        'status' : 401,
+                    })
+        else:
+            return Response(status= status.HTTP_400_BAD_REQUEST, data= {
+                'error' : 'Formulário de usuário não foi preenchido corretamente',
+                'status' : 400,
+            })
--- auth/views.py
+++ auth/views.py
@@ -1,19 +1,12 @@
 from django.db import models
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.response import Response
-from auth.models import BearerAuthentication
 from auth.controllers import AuthController
 
 class AuthComToken(ObtainAuthToken):
 
     controller = AuthController()
-    
+
     def post(self, request, *args, **kwargs):
 
-        token, user = self.controller.authenticate(self, request)
+        return self.controller.authenticate(self, request)
 
-        return Response({
-            'user-id' : user.pk,
-            'token' : token.key,
-            'token-type' : BearerAuthentication.keyword,
-        })
