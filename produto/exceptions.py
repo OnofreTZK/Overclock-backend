5bbce65c73d8440692dc83a010977ff4f200a58f
Amanda Albuquerque <e_amandafreire@jfrn.jus.br>
2021-07-31 19:01:00 -0300

    criando service de produto

---
+++ produto/exceptions.py
@@ -0,0 +1,9 @@
+
+class InvalidNameExist(Exception):
+    pass
+
+class InvalidDiscount(Exception):
+    pass
+
+class InvalidPrice(Exception):
+    pass
---
+++ produto/services.py
@@ -0,0 +1,42 @@
+from produto.models import Produto
+
+class ProdutoService(object):
+
+    nome_produto = ""
+    preco_produto = int
+    promocao = bool
+    desconto = float
+    caminho_imagem = ""
+
+    def __init__(self, nome_produto, preco_produto, promocao, desconto, caminho_imagem):
+        self.nome_produto = nome_produto
+        self.preco_produto = preco_produto
+        self.promocao = promocao
+        self.desconto = desconto
+        self.caminho_imagem = caminho_imagem
+
+
+    def validate_name(self):
+        if Produto.objects.filter(nome_produto=self.nome_produto).exists():
+            return False
+        else:
+            return True
+
+    def validate_discount(self):
+        if self.promocao:
+            if self.desconto >= 1 and self.desconto <= 90:
+                return True
+            else:
+                return False
+        else:
+            if self.desconto == 0:
+                return True
+            else:
+                return False
+
+    def validate_price(self):
+        if self.preco_produto > 0:
+            return True
+        else:
+            return False
+
